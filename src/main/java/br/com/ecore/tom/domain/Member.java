7e5e577ee4f99ca8c392bdaa55442459b3886f9b
Lucio Leandro <lucio.leo85@gmail.com>
2022-06-04 14:53:20 -0300

    Refatorando modelos

--- src/main/java/br/com/ecore/tom/domain/Member.java
+++ src/main/java/br/com/ecore/tom/domain/Member.java
@@ -2,16 +2,28 @@ package br.com.ecore.tom.domain;
 
 import java.util.UUID;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Member {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotNull
   private UUID uuid;
 
-  public Member(long id, UUID uuid) {
+  @NotNull
+  private String displayName;
+
+  public Member(long id, UUID uuid, String displayName) {
     this.id = id;
     this.uuid = uuid;
+    this.displayName = displayName;
   }
 
   public Long getId() {
@@ -20,6 +32,10 @@ public class Member {
 
   public UUID getUuid() {
     return uuid;
+  }
+
+  public String getDisplayName() {
+    return displayName;
   }
 
 }
--- src/main/java/br/com/ecore/tom/domain/Team.java
+++ src/main/java/br/com/ecore/tom/domain/Team.java
@@ -2,16 +2,28 @@ package br.com.ecore.tom.domain;
 
 import java.util.UUID;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Team {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotNull
   private UUID uuid;
 
-  public Team(long id, UUID uuid) {
+  @NotNull
+  private String name;
+
+  public Team(long id, UUID uuid, String name) {
     this.id = id;
     this.uuid = uuid;
+    this.name = name;
   }
 
   public Long getId() {
@@ -20,6 +32,10 @@ public class Team {
 
   public UUID getUuid() {
     return uuid;
+  }
+
+  public String getName() {
+    return name;
   }
 
 }
