6773a830bb90f22244115354e59deef9e59634f5
Lúcio Leandro <lucio.leo85@gmail.com>
2022-06-17 16:07:14 -0300

    Removing Junit4 from classpath

--- src/test/java/br/com/ecore/tom/service/RoleServiceTest.java
+++ src/test/java/br/com/ecore/tom/service/RoleServiceTest.java
@@ -1,8 +1,8 @@
 package br.com.ecore.tom.service;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
